216c7fe038a9cc06da1f7bd9d76b5abc9868be7c
MiguelAngelfnc <miguelmb3112@gmail.com>
2023-12-01 18:31:44 +0100

    ejercicio 1

---
+++ Ejercicios Javascript advanced/Review-curso-introductorio/Conditional-and-loops/Conditional-and-loops.js
@@ -0,0 +1,37 @@
+/*Create a function named calculateSalary which takes an input parameter, and calculates and returns the corresponding salary. 
+You must use the switch switch statement. For each role the corresponding salary is as follows:*/
+
+//ceo => 2200€
+//manager => 1800€
+//cto => 1800€
+//developer => 1500€
+//default => 1000€
+
+function calculateSalary(role) {
+    switch(role){
+        case "ceo":
+          return 2200;
+        case "manager":
+          return 1800;
+        case "cto":
+          return 1800;
+        case "developer":
+          return 1500;
+        case "other":
+          return 1000;           
+
+    }
+  
+}
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
