fa843c9eebc2381a21c0cae510b24028040b0799
MiguelAngelfnc <miguelmb3112@gmail.com>
2024-01-14 15:10:43 +0100

    primer-ejercicio

---
+++ Ejercicios Javascript advanced/Features/Jason-Method/jason-1.js
@@ -0,0 +1,17 @@
+//Convert the developer object into JSON
+
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+console.log(developer);
+// Print developer as JSON object
+
+const developstring= JSON.stringify(developer);
+console.log(developstring);
